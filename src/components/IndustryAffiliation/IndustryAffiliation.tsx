e1ffcb5bc9aff282800e253d7607d0dc67ec559d
Fenil Kadhiwala <fkadhiwa@stevens.edu>
2025-08-14 11:20:25 -0400

    fix:industry adffiliation responsiveness

--- src/components/IndustryAffiliation/IndustryAffiliation.tsx
+++ src/components/IndustryAffiliation/IndustryAffiliation.tsx
@@ -2,16 +2,30 @@ import { IndustryAffiliationOptions } from "@/utils/constants";
 import { Image } from "@mantine/core";
 import Link from "next/link";
 import React from "react";
-import { AnimatedText } from "../CommonComponents/AnimatedText";
 
 export const IndustryAffiliation = () => {
   return (
     <div className="bg-[#F9F5F0] py-10 mt-10">
-      <div className="flex justify-center items-center gap-20">
+      <div
+        className="
+          grid 
+          grid-cols-2 gap-8 
+          sm:flex sm:flex-wrap sm:justify-center sm:items-center sm:gap-20
+        "
+      >
         {IndustryAffiliationOptions?.map((item: any, index: number) => {
           return (
-            <Link key={index} target="_blank" href={item?.link}>
-              <Image h={140} w={160} fit="contain" src={item?.logo} />
+            <Link
+              key={index}
+              target="_blank"
+              href={item?.link}
+              className="flex justify-center items-center"
+            >
+              <img
+                src={item?.logo}
+                className="h-[90px] w-[100px] md:h-[140px] md:w-[160px] object-contain"
+                alt=""
+              />
             </Link>
           );
         })}
