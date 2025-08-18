34044bca253d90e3a64b53cb08e3c627503e6cc2
Fenil Kadhiwala <fkadhiwa@stevens.edu>
2025-08-18 15:59:11 -0400

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://bvgems.com", // your live domain
+  generateRobotsTxt: true, // generate robots.txt file as well
+  sitemapSize: 7000, // (optional) split large sitemaps
+  changefreq: "daily",
+  priority: 0.7,
+};
