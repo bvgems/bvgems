f60833d55f9cac07479bdfb5413d8bfa9f6bb8bb
Fenil Kadhiwala <fkadhiwa@stevens.edu>
2025-08-22 16:48:20 -0400

    url change

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
       },
       {
         source: "/sapphire",
-        destination: "sapphires", 
+        destination: "/sapphire", 
         permanent: true,
       },
     ];
